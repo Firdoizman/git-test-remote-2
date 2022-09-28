2bb091c09a4bb4ffb39e2be4b3963743b591bd40
Firdoizman <firdo.yakuzy69@gmail.com>
2022-09-28 17:35:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eos consectetur quod, fuga velit dicta magnam commodi voluptates ea accusamus quae cum dolor consequatur sunt delectus. In error et provident!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
